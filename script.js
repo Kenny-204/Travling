06a69a18f36e0fb7b14d4e7cab1603e0f59b83fa
Kenny-204 <giwatwins@gmail.com>
2024-06-23 17:28:11 +0100

    added new sections

--- script.js
+++ script.js
@@ -11,14 +11,39 @@ const closeDropdown = function () {
 }
 const dropdown = function () {
     toggleBtn.addEventListener('click', function () {
-        closeDropdown()     
+        closeDropdown()
     })
     document.querySelector('body').addEventListener('click', function (e) {
         const dropdown = e.target.closest('.dropdown--menu')
         // if(!dropdown)       
-            // console.log(dro)
-            // if (e.target.parentElement.classList.contains('dropdown--menu') || e.target.classList.contains('fa-solid') || e.target.classList.contains("dropdown--item"))  return 
+        // console.log(dro)
+        // if (e.target.parentElement.classList.contains('dropdown--menu') || e.target.classList.contains('fa-solid') || e.target.classList.contains("dropdown--item"))  return 
         // closeDropdown()
     })
 }
 dropdown()
+
+// sticky navbar
+const nav = document.querySelector('.navbar')
+const header = document.querySelector('header')
+
+const navHeight = nav.getBoundingClientRect().height;
+const headObserve = function (entries) {
+    const [entry] = entries
+    if (!entry.isIntersecting) {
+        nav.classList.add('sticky')
+        console.log(navHeight)
+    }
+    else {
+        nav.classList.remove('sticky')
+    }
+
+    console.log(entry)
+}
+const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+}
+const observer = new IntersectionObserver(headObserve, options)
+observer.observe(header)
